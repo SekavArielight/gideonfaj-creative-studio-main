d1d5b703f63250bd11bb483efb3f363d31569202
God'swill Sekav Apya <godswillapya@gmail.com>
2025-01-12 21:31:51 +0100

    Customizations

--- src/constants/index.js
+++ src/constants/index.js
@@ -39,22 +39,22 @@ import {
   export const navigation = [
     {
       id: "0",
-      title: "Features",
-      url: "#features",
+      title: "Services",
+      url: "#services",
     },
     {
       id: "1",
-      title: "Pricing",
+      title: "Contact",
       url: "#pricing",
     },
     {
       id: "2",
-      title: "How to use",
+      title: "About",
       url: "#how-to-use",
     },
     {
       id: "3",
-      title: "Roadmap",
+      title: "Learn",
       url: "#roadmap",
     },
     {
