3d8b77871a95f70213156328961d529cf1796518
God'swill Sekav Apya <godswillapya@gmail.com>
2025-01-13 17:37:16 +0100

    Commit Streak

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-          Embark on your journey with us as your trusted guide, every step of the way
+          Embark on your journey with us as your trusted guide, every step of the way.
           </p>
           <Button href="/pricing" white>
             Get Started
